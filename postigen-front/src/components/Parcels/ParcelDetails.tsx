9a84c047474ef52d4f4413726a8c593544db9891
vlad <vladislavs@codnity.com>
2023-10-08 16:31:30 +0300

    Add put into locker logic

--- postigen-front/src/components/Parcels/ParcelDetails.tsx
+++ postigen-front/src/components/Parcels/ParcelDetails.tsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../api';
-import { Parcel } from "../../../types/common";
+import { Parcel, Locker } from "../../../types/common";
 import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography";
 import { LinearIndeterminate } from "../Global/LoadingBar";
 import BackButton from "../Global/Buttons/BackButton";
+import Button from '@mui/material/Button';
+import { Alert, Select, MenuItem } from "@mui/material";
 import { Grid } from '@mui/material';
 
 function ParcelDetails(): React.ReactElement {
     const { parcelId } = useParams();
     const [parcel, setParcel] = useState<Parcel | null>(null);
+    const [lockers, setLockers] = useState<Locker[]>([]);
+  const [selectedLocker, setSelectedLocker] = useState<number | null>(null);
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
     useEffect(() => {
         api.get<Parcel>(`/api/parcels/${parcelId}/`)
@@ -20,7 +26,40 @@ function ParcelDetails(): React.ReactElement {
             .catch((error) => {
                 console.error(`Error fetching parcel ${parcelId}:`, error);
             });
+
+        api.get<Locker[]>('/api/lockers/')
+            .then((response) => {
+                setLockers(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching lockers:', error);
+            });
     }, [parcelId]);
+
+    const handlePutParcelIntoLocker = async () => {
+        if (selectedLocker !== null) {
+            try {
+                const response = await api.put(
+                    `/api/parcels/${parcelId}/put-parcel/`,
+                {"locker_id": selectedLocker},
+                );
+                if (response.status === 200) {
+                    setSuccessMessage('Parcel put into locker successfully');
+                    setParcel((prevParcel) => ({
+                        ...(prevParcel as Parcel),
+                        locker: selectedLocker,
+                    }));
+                } else {
+                    setErrorMessage('Operation was not successful');
+                }
+            } catch (error) {
+                console.error('Error putting parcel into locker:', error);
+                setErrorMessage('Operation was not successful');
+            }
+        } else {
+            setErrorMessage('Please select a locker');
+        }
+    };
 
     if (!parcel) {
         return <LinearIndeterminate/>;
@@ -62,7 +101,30 @@ function ParcelDetails(): React.ReactElement {
                     <div>
                         <h2>Locker ID: {parcel.locker}</h2>
                     </div>
-                ) : null}
+                ) : (
+                    <div>
+                        <Select
+                            value={selectedLocker}
+                            onChange={(e) => setSelectedLocker(e.target.value as number)}
+                            placeholder="Select a locker"
+                        >
+                            {lockers.map((locker) => (
+                                <MenuItem key={locker.id} value={locker.id}>
+                                    {locker.location_address} - {locker.size} - {locker.status}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handlePutParcelIntoLocker}
+                        >
+                            Put parcel into a locker
+                        </Button>
+                    </div>
+                )}
+                {successMessage && <p><Alert severity="success">{successMessage}</Alert></p>}
+                {errorMessage && <p><Alert severity="error">{errorMessage}</Alert></p>}
             </div>
         </Box>
     );
